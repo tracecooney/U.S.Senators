fda8af3dd7f16ff1f19e7346f14c58020336af37
Trace Cooney <trace.wade.cooney@gmail.com>
2024-04-21 22:34:32 -0600

    Fixed JS and added CSS style.

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,49 @@
 // Import senators data
 import { senators } from '../data/senators.js';
+console.log(senators);
 
 // Tag HTML elements
 const myParent = document.querySelector('#peopleHere');
 
-// Check if parent element exists
-if (!myParent) {
-    console.error("Parent element not found");
-} else {
-    // Loop through all the senators
-    function displaySenators(senators) {
-        // Clear previous content
-        myParent.innerHTML = "";
+// Loop through all the senators
+function displaySenators(senators) {
+    myParent.innerHTML = "";
+    senators.forEach(senator => {
+        const myFigure = document.createElement('figure');
 
-        senators.forEach(senator => {
-            // Create figure element
-            const myFigure = document.createElement('figure');
+        const myImage = document.createElement('img');
+        
+        // Retrieve govtrack_id for each senator
+        const govtrackId = senator.govtrack_id;
+        
+        // Construct the image URL using govtrack_id
+        myImage.src = `https://www.govtrack.us/static/legislator-photos/${govtrackId}-200px.jpeg`;
+        myImage.alt = senator.name;
 
-            // Create image element
-            const myImage = document.createElement('img');
-            
-            // If senator.url contains only the ID
-            myImage.src = `https://www.govtrack.us/static/legislator-photos/${senator.url}.jpeg`;
+        // Create caption element
+        const myCaption = document.createElement('figcaption');
+        myCaption.textContent = senator.name;
 
-            myImage.alt = senator.name;
+        // Assign gender class
+        switch (senator.gender) {
+            case "female":
+                myFigure.className = "female";
+                break;
+            case "male":
+                myFigure.className = "male";
+                break;
+            default:
+                myFigure.className = "other";
+        }
 
-            // Create caption element
-            const myCaption = document.createElement('figcaption');
-            myCaption.textContent = senator.name;
+        // Assemble the parts
+        myFigure.appendChild(myImage);
+        myFigure.appendChild(myCaption);
 
-            // Assign gender class
-            switch (senator.gender) {
-                case "female":
-                    myFigure.className = "female";
-                    break;
-                case "male":
-                    myFigure.className = "male";
-                    break;
-                default:
-                    myFigure.className = "other";
-            }
-
-            // Assemble the parts
-            myFigure.appendChild(myImage);
-            myFigure.appendChild(myCaption);
-
-            // Attach the HTML to parent element
-            myParent.appendChild(myFigure);
-        });
-    }
-
-    // Call the function to display senators
-    displaySenators(senators);
+        // Attach the HTML to parent element
+        myParent.appendChild(myFigure);
+    });
 }
+
+// Call the function to display senators
+displaySenators(senators);
